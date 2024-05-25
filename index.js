8cfdfa24d14d34602afd8388fbbb6f9d12b47fe8
Unata Kenneth Oche <unatakennethoche@gmail.com>
2024-05-25 09:38:14 +0100

    change some css

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const dayInput=document.getElementById("day");
 const monthInput=document.getElementById("month");
 const yearInput=document.getElementById("year");
 const calculateBtn=document.getElementById("calculate-btn")
+const dayInputValue=dayInput.value.trim();
+const monthInputValue=monthInput.value.trim();
+const yearInputValue=yearInput.value.trim();
+const errorSpan=document.querySelectorAll(".error-message")
+const invalidStyle=()=>{
+const label=document.querySelectorAll(".label")
+label.forEach((list)=>{
+list.classList.add("error-label")
+})
+  }
 
 const validateInputs=(e)=>{
   e.preventDefault()
   
-const dayInputValue=dayInput.value.trim();
-const monthInputValue=monthInput.value.trim();
-const yearInputValue=yearInput.value.trim();
-
+  
 if(dayInputValue===""){
+errorSpan[0].textContent="This field is required"
+invalidStyle()
+}
+}
 
-}
-}
+
 
 calculateBtn.addEventListener("click",validateInputs)
